352570134657ac875a70c7685de2c6f2d3a5aa93
Atom <108768353+Alfacer1@users.noreply.github.com>
2024-02-03 14:11:06 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -28,10 +28,8 @@ function doMath(x, znak, y) {
     console.log(`Результат: ${result}`);
 }
 
-// Зчитування чисел і оператора від користувача
 let x = parseFloat(prompt("Введіть перше число:"));
 let znak = prompt("Введіть математичний оператор (+, -, *, /, %, ^):");
 let y = parseFloat(prompt("Введіть друге число:"));
 
-// Виклик функції
 doMath(x, znak, y);
