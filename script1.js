7434769235c11a2ceb27c11bdf5a8c59aa1360af
Atom <108768353+Alfacer1@users.noreply.github.com>
2024-02-03 14:10:45 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -10,13 +10,13 @@ function averageOfNumbers(arr) {
     }
 
     if (count === 0) {
-        return 0; // Якщо в масиві немає чисел, повертаємо 0, щоб уникнути ділення на 0.
+        return 0;
     }
 
     return sum / count;
 }
 
-// Приклад використання
+// Приклад
 let mixedArray = [1, 'abc', 2, 'def', 3, 4, 'ghi'];
 let result = averageOfNumbers(mixedArray);
 console.log(`Середнє арифметичне числових елементів: ${result}`);
