da6a55c1674d93feca42a168455e021c9ce14b11
Atom <108768353+Alfacer1@users.noreply.github.com>
2024-02-03 20:10:37 +0200

    Update script2.js

--- script1.js
+++ script1.js
@@ -1,9 +1,8 @@
 function averageOfNumbers(arr) {
     let sum = 0;
     let count = 0;
-
     for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === 'number') {
+        if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
             sum += arr[i];
             count++;
         }
@@ -17,6 +16,6 @@ function averageOfNumbers(arr) {
 }
 
 // Приклад
-let mixedArray = [1, 'abc', 2, 'def', 3, 4, 'ghi'];
+let mixedArray = [1, 'abc', NaN, 2, 'def', 3341, 4, 'ghi'];
 let result = averageOfNumbers(mixedArray);
 console.log(`Середнє арифметичне числових елементів: ${result}`);
